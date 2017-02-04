8a25ce207a48461b9bdef5b8225caf5b90bc8591
Kevin Charter <kevin.charter@acm.org>
2017-02-04 13:46:03 -0700

    Set up to deploy on Heroku with a Postgres DB
    To deploy to Heroku, we'll need to use Postgres in
    production. Unfortunately, I've also discovered that if the `sqlite3`
    gem is in your `Gemfile`, the installation on Heroku fails. So, let's
    switch to using Postgres in development and test too.
    
    It turns out that once you get Postgres running, the setup is actually
    quite easy, and you don't need any database passwords in your
    `config/database.yml` file.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161128190308) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
