a03bf9386133b1c90d809868a4f4aa74630b504e
Kim Lew <kimlew451@gmail.com>
2022-10-22 14:22:55 -0600

    Adds versions, removes apt commands

--- init_and_run_app.sh
+++ init_and_run_app.sh
@@ -1,35 +1,25 @@
 #! /usr/bin/env bash
 
-# This script installs packages needed in environment to run rails_see_stuff.
-#
-# mkdir rails_see_stuff
-# cd rails_see_stuff
-# Check if in rails_see_stuff & if not: cd rails_see_stuff
+# This script run the See Stuff application.
+# Maybe: installs packages needed in environment to run rails_see_stuff.
 
-sudo apt update && apt install -y \
-  nodejs \
-  nano \
-  rbenv
+# Check if rbenv is installed on system. If not: exit 1 & prompt user with msg:
+# if rbenv -v != mac$ rbenv --version rbenv 1.2.0; then
+#   echo 'You must install the package manager, rbenv version 1.2.0'
+# end
+
+# Check if in rails_see_stuff & if not: cd rails_see_stuff
+# if current directory is not rails_see_stuff; then
+#   cd rails_see_stuff
+# end
 
 rbenv local 3.0.1
-gem install rails -v 7
+gem install rails -v 7.0
 gem install bundler -v 2.3.22
-gem install puma
+gem install puma -v 5.6.4
 gem install sqlite3
-gem install actionpack
+gem install actionpack -v 7.0.4
 bundle install
-
-# First, copy locked Gemfile & Gemfile.lock & only installs missing gems.
-# Then copy everything from app's current directory on host into app container's
-# root directory. This copy includes locked Gemfile & Gemfile.lock, so
-# container has app code AND correct dependencies. Can use . or $INSTALL_PATH
-# for current dir since WORKDIR set to /opt/rails7_task_manager.
-# bundle check || bundle install - checks that the gems are not already installed before installing them.
-# Copying these files as an independent step, followed by bundle install, means that the project gems do not need to be rebuilt every time you make changes to your application code. This will work in conjunction with the gem volume that we will include in our Compose file, which will mount gems to your application container in cases where the service is recreated but project gems remain the same.
-# See: https://www.digitalocean.com/community/tutorials/containerizing-a-ruby-on-rails-application-for-development-with-docker-compose
-cp Gemfile .
-cp Gemfile.lock .
-bundle check || bundle install
 
 rake db:create
 rake db:migrate
