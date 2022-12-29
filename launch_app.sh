24a885e88d2758d9375e0e0eb3985071ff96f6b2
Kim Lew <kimlew451@gmail.com>
2022-12-29 10:11:05 -0700

    Changes to right folder, adds rails commands, adds checks

--- launch_app.sh
+++ launch_app.sh
@@ -17,29 +17,52 @@ check_current_directory () {
   fi
 }
 
-check_if_rbenv_installed () {
-  if ! command -v rbenv > /dev/null; then
-    echo 'You must install the package manager, rbenv. For example, install with:'
-    echo 'sudo apt install rbenv OR'
-    echo 'brew install rbenv ruby-build'
-    exit 1
-  fi
-}
-
+echo "CLONING See Stuff web app from GitHub..."
+# BAD since no Docker & Docker Compose: git clone --depth 1 --branch v2.0 https://github.com/kimlew/rails_see_stuff
+# BETTER with Docker & Docker Compose: git clone <this branch>
+# BEST & Later: Push image to DockerHub.com & don't need git clone step
+cd "${APP_DIR}"
 check_current_directory
-check_if_rbenv_installed
+echo
 
-# Use specific version of Ruby for project that works with Rails 7.
-# Install bundler which is required for rake commands for the database & items.
-rbenv local 3.1.2
-eval "$(rbenv init -)"
-gem install bundler -v 2.3.22
+echo "GLOBAL/system Ruby version is: "
+ruby -v
+echo
+
+# Install bundler which is required for rails commands for the database & items & rails.
+echo "INSTALLING bundler..."
+echo "gem: --no-document" > ~/.gemrc
+sudo gem install rails -v 7 && sudo gem install bundler -v 2.3.22
+echo
+echo "SHOW Ruby location & PATH. Should NOT be in SHIMS, since no rbenv."
+which ruby
+echo "$PATH"
+echo
+# echo "CHECK write permissions at stored ruby location to make sure CAN update SHIMS: "
+# ls -lah /home/ubuntu/.rbenv/shims/ruby
+# If you're using rbenv, you must make the rails executable available (by updating the shims) with: rbenv rehash
 bundle install
 
-rake db:drop
-rake db:create
-rake db:migrate
-rake db:seed
+echo "CHECK where gems are installed with the home argument with: gem env home"
+gem env home
+echo
+echo "CHECK the PATH with gem env: "
+gem env
 
-rails server
-# View in browser at: http://localhost:3000
+echo
+echo "SHOW Rails version..."
+rails -v
+echo
+
+echo "RUNNING rails commands and starting the app..."
+# Create the new db, load the schema, & seed the db.
+# rake db:drop
+# rake db:create
+rails db:migrate RAILS_ENV=development
+rails db:seed
+echo
+
+rails server -b 0.0.0.0
+# See web app in browser:
+# - locally with Docker Compose (port forwarded 3000 in container to 48017): <http://localhost:48017/>
+# - on web host with AWS EC2 instance, at IP address, e.g., https://54.190.12.61/
