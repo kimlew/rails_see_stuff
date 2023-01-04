bbd4ff92a0e4abb07a08f024e215255a89fb3360
Kim Lew <kimlew451@gmail.com>
2023-01-04 11:18:52 -0700

    Removes install of rails 7 & bundler since to go in Dockerfile

--- launch_app.sh
+++ launch_app.sh
@@ -9,49 +9,21 @@
 
 set -e
 
+# TODO: Need to clone to root of Docker container, not root of Ubuntu machine
+# TODO: OR push Dockerfile & docker-compose.yml to DockerHub.
+# APP_DIR="/home/ubuntu/rails_see_stuff"
+APP_DIR="/opt/rails_see_stuff"
+
 check_current_directory () {
   if [[ ! -d '../rails_see_stuff' ]]; then
     echo 'You must be in the directory, rails_see_stuff, after cloning the code'
-    echo 'from https://github.com/kimlew/rails_see_stuff'
+    echo 'with: git clone -b see-stuff-in-docker git@github.com:kimlew/rails_see_stuff.git'
     exit 1
   fi
 }
 
-echo "CLONING See Stuff web app from GitHub..."
-# BAD since no Docker & Docker Compose: git clone --depth 1 --branch v2.0 https://github.com/kimlew/rails_see_stuff
-# BETTER with Docker & Docker Compose: git clone <this branch>
-# BEST & Later: Push image to DockerHub.com & don't need git clone step
 cd "${APP_DIR}"
 check_current_directory
-echo
-
-echo "GLOBAL/system Ruby version is: "
-ruby -v
-echo
-
-# Install bundler which is required for rails commands for the database & items & rails.
-echo "INSTALLING bundler..."
-echo "gem: --no-document" > ~/.gemrc
-sudo gem install rails -v 7 && sudo gem install bundler -v 2.3.22
-echo
-echo "SHOW Ruby location & PATH. Should NOT be in SHIMS, since no rbenv."
-which ruby
-echo "$PATH"
-echo
-# echo "CHECK write permissions at stored ruby location to make sure CAN update SHIMS: "
-# ls -lah /home/ubuntu/.rbenv/shims/ruby
-# If you're using rbenv, you must make the rails executable available (by updating the shims) with: rbenv rehash
-bundle install
-
-echo "CHECK where gems are installed with the home argument with: gem env home"
-gem env home
-echo
-echo "CHECK the PATH with gem env: "
-gem env
-
-echo
-echo "SHOW Rails version..."
-rails -v
 echo
 
 echo "RUNNING rails commands and starting the app..."
