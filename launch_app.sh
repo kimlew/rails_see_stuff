96ec35c8df084e60256a52903e9b764d94853055
Kim Lew <kimlew451@gmail.com>
2023-01-19 10:10:15 -0700

    Removes unneeded function, check_current_directory

--- launch_app.sh
+++ launch_app.sh
@@ -9,21 +9,10 @@
 
 set -e
 
-# APP_DIR="/home/ubuntu/rails_see_stuff" # This is root of AWS EC2 instance.
 APP_DIR="/opt/rails_see_stuff" # This is root of Docker container.
 
-check_current_directory () {
-  if [[ ! -d '../rails_see_stuff' ]]; then
-    echo 'You must be in the directory, rails_see_stuff, after cloning the code'
-    echo 'with: git clone -b see-stuff-in-docker git@github.com:kimlew/rails_see_stuff.git'
-    exit 1
-  fi
-}
-
 cd "${APP_DIR}"
-check_current_directory
 echo
-
 echo "RUNNING rails commands and starting the app..."
 # Create the new db, load the schema, & seed the db.
 # rake db:drop
